08700ba365f615028756f5034fde8d1270a3c5a8
jisoo <wlttnrla@gmail.com>
2024-09-25 15:15:20 +0900

    Title: 메이즈 러너, Time: 108ms, Memory: 9MB, Status: Passed - CodeTree

--- 240925/메이즈 러너/maze-runner.java
+++ 240925/메이즈 러너/maze-runner.java
@@ -1,10 +1,5 @@
-package samsung;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 class Pair{
     int x;
